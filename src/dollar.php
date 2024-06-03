f1e1a374c25287f2b4ddda0a49b72387277f2689
furuya <furuya.to@uhmfg.co.jp>
2024-06-03 23:03:49 +0900

    [feat]equals()

--- src/dollar.php
+++ src/dollar.php
@@ -12,4 +12,18 @@ class Dollar{
   {
     return new Dollar($this->amount * $multiplier);
   }
+
+  public function equals(object $object)
+  {
+    $dollar = $this::cast($object);
+    return $this->amount === $dollar->amount;
+  }
+
+  public static function cast($obj): self
+  {
+    if (!$obj instanceof self) {
+      throw new InvalidArgumentException("{$obj} is not instance of DollarObject");
+    }
+    return $obj;
+  }
 }
