7867b6180aebcb4dae1c797410b99d58850aba58
furuya <furuya.to@uhmfg.co.jp>
2024-06-09 21:29:44 +0900

    [refactor]timesメソッドをmoneyクラスに引き上げ

--- src/tests/MoneyTest.php
+++ src/tests/MoneyTest.php
@@ -11,8 +11,8 @@ class MoneyTest extends TestCase
   public function testMultiPlication()
   {
     $five = Money::dollar(5);
-    $this->assertEquals(Money::dollar(10), $five->times(2));
-    $this->assertEquals(Money::dollar(15), $five->times(3));
+    $this->assertTrue((Money::dollar(10))->equals($five->times(2)));
+    $this->assertTrue((Money::dollar(15))->equals($five->times(3)));
   }
 
   public function testEquality()
@@ -29,13 +29,18 @@ class MoneyTest extends TestCase
   public function testFrancMultiPlication()
   {
     $five = Money::franc(5);
-    $this->assertEquals(Money::franc(10), $five->times(2));
-    $this->assertEquals(Money::franc(15), $five->times(3));
+    $this->assertTrue((Money::franc(10))->equals($five->times(2)));
+    $this->assertTrue((Money::franc(15))->equals($five->times(3)));
   }
 
   public function testCurrency()
   {
     $this->assertEquals("USD", Money::dollar(1)->currency());
     $this->assertEquals("CHF", Money::franc(1)->currency());
+  }
+
+  public function testDifferentClassEquality()
+  {
+    $this->assertTrue((new Money(10, "CHF"))->equals(new Franc(10,"CHF")));
   }
 }
