7007163679df659ecd4955a5630261d4034a8d35
furuya <furuya.to@uhmfg.co.jp>
2024-06-12 21:32:29 +0900

    [test]SumクラスのテストとBankクラスのreduceテスト

--- src/tests/MoneyTest.php
+++ src/tests/MoneyTest.php
@@ -4,6 +4,7 @@ use \PHPUnit\Framework\TestCase;
 
 require_once(dirname(__FILE__) ."/../money.php");
 require_once(dirname(__FILE__) ."/../bank.php");
+require_once(dirname(__FILE__) ."/../sum.php");
 
 class MoneyTest extends TestCase
 {
@@ -43,6 +44,21 @@ class MoneyTest extends TestCase
     $sum = self::castSum($result);
     $this->assertEquals($five, $sum->augend);
     $this->assertEquals($five, $sum->addend);
+  }
+
+  public function testReduceSum()
+  {
+    $sum = new Sum(Money::dollar(3), Money::dollar(4));
+    $bank = new Bank();
+    $result = $bank->reduce($sum, "USD");
+    $this->assertEquals(Money::dollar(7), $result);
+  }
+
+  public function testReduceMoney()
+  {
+    $bank = new Bank();
+    $result = $bank->reduce(Money::dollar(1), "USD");
+    $this->assertEquals(Money::dollar(1), $result);
   }
 
   private static function castSum($obj): Sum
