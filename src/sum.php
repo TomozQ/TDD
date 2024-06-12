d2f4aa8dc53969b10ed414c084ab9562a11b70e7
furuya <furuya.to@uhmfg.co.jp>
2024-06-12 21:35:29 +0900

    [feat]足し算メソッド

--- src/sum.php
+++ src/sum.php
@@ -1,8 +1,21 @@
 <?php
 
-require_once(dirname(__FILE__) ."/../money.php");
+require_once(dirname(__FILE__) ."/money.php");
+require_once(dirname(__FILE__) ."/Expression.php");
 
-class Sum {
+class Sum implements Expression{
   public Money $augend;
   public Money $addend;
+
+  public function __construct(Money $augend, Money $addend) 
+  {
+    $this->augend = $augend;
+    $this->addend = $addend;
+  }
+
+  public function reduce(string $to): Money
+  {
+    $amount = $this->augend->amount() + $this->addend->amount();
+    return new Money($amount, $to);
+  }
 }
