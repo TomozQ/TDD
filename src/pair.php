bd630edc906b376fd1fdb425f7555b7d0727669d
WSL2 Ubuntu Furuya <inf.4clover@gmail.com>
2024-06-15 06:28:33 +0900

    [feat]Pairクラス作成

---
+++ src/pair.php
@@ -0,0 +1,12 @@
+<?php
+
+class Pair
+{
+  private string $from;
+  private string $to;
+
+  public function __construct(string $from, string $to) {
+    $this->from = $from;
+    $this->to = $to;
+  }
+}
